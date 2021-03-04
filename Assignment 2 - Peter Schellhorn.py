a98cdbdb6dbd6af2b51294a374fba65533bde260
Peter Schellhorn <78440560+PeterSchell11@users.noreply.github.com>
2021-03-03 19:18:05 -0500

    Add files via upload

---
+++ Assignment 2 - Peter Schellhorn.py
@@ -0,0 +1,16 @@
+#Assignment 1
+
+#Problem 1
+
+A="Welcome,"
+B="Peter Schellhorn"
+print(A+' '+B+'!\n')
+
+#problem 2
+
+x="Facebook's rating is "
+fbr=float(3.5)
+fbrs=str(fbr)
+fb=x + fbrs
+
+print(fb)
---
+++ Assignment 3 - Peter Schellhorn.py
@@ -0,0 +1,19 @@
+# Peter Schellhorn
+# Assignment 3
+
+# Question 1
+
+name = input('Please enter name ')
+age = int(input('Please enter age '))
+income = float(input('Please enter income? '))
+
+print ('Your name is:', name,'\n Your age: is ', age,'\n Your income is: ', income)
+
+
+# Question 2
+
+value_1 = 971825.12334
+print(f'Value one rounded to three decimals is {value_1:.3f}')
+
+value_2 =6734911444
+print(f'Value two with commas {value_2:,}')
